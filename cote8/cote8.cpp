4a187d2d665e22d5a208311eeb6f9c3c410bc101
ohne <ghdlr785@gmail.com>
2022-06-20 11:15:44 +0900

    초기 커밋.

---
+++ cote8/cote8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "Even";
+    string answer2 = "Odd";
+    if (num % 2 == 0)
+    {
+        return answer;
+    }
+    else
+    {
+        return answer2;
+    }
+}
+
+int main()
+{
+    
+}
